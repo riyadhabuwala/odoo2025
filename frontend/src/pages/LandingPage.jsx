8df2394acafaa9de52c826c73baa2cc1e4dfa7e5
TanishaD21 <tanisharkd@gmail.com>
2025-07-12 11:37:00 +0530

    demoReact

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function LandingPage() {
+  return (
+   <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Sustainable Clothing Brand 🌿</h1>
+      <p>This is the landing page. More coming soon!</p>
+    </div>
+  )
+}
+
+export default LandingPage
